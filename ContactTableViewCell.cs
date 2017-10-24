e2a817b83455855c45ba863612c3e66097877d24
astrobirb <31900770+astrobirb@users.noreply.github.com>
2017-10-24 14:00:19 -0600

    End of class
    was going to modify Main.storyboard but didn't have enough time

--- ContactTableViewCell.cs
+++ ContactTableViewCell.cs
@@ -6,8 +6,19 @@ namespace ContactList
 {
     public partial class ContactTableViewCell : UITableViewCell
     {
+
+		private Contact contactData;
+		public Contact ContactData
+		{
+            get { return contactData; }
+			set {
+                contactData = value;
+            }
+		}
+
         public ContactTableViewCell (IntPtr handle) : base (handle)
         {
+            
         }
     }
 }
