ce5cbf022ccb7ff1be63ca3a0a1965d13004d2cd
astrobirb <31900770+astrobirb@users.noreply.github.com>
2017-10-27 11:57:11 -0600

    Almost complete

--- DetailsViewController.cs
+++ DetailsViewController.cs
@@ -6,8 +6,19 @@ namespace ContactList
 {
     public partial class DetailsViewController : UIViewController
     {
+
+        public Contact selectedContact;
+
         public DetailsViewController (IntPtr handle) : base (handle)
         {
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            imgContactImage.Image = UIImage.FromFile(selectedContact.ImagePath);
+            labelContactName.Text = selectedContact.Name;
+            labelContactNumber.Text = selectedContact.Number.ToString();
+            labelContactLocation.Text = selectedContact.Location;
         }
     }
 }
--- DetailsViewController.designer.cs
+++ DetailsViewController.designer.cs
@@ -14,5 +14,43 @@ namespace ContactList
     [Register ("DetailsViewController")]
     partial class DetailsViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView imgContactImage { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel labelContactLocation { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel labelContactName { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel labelContactNumber { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (imgContactImage != null) {
+                imgContactImage.Dispose ();
+                imgContactImage = null;
+            }
+
+            if (labelContactLocation != null) {
+                labelContactLocation.Dispose ();
+                labelContactLocation = null;
+            }
+
+            if (labelContactName != null) {
+                labelContactName.Dispose ();
+                labelContactName = null;
+            }
+
+            if (labelContactNumber != null) {
+                labelContactNumber.Dispose ();
+                labelContactNumber = null;
+            }
+        }
     }
 }
